9ea58103f5eaac7225ad7a69231a7d90f311712f
Harpal <hpanwar@grepruby.com>
2016-05-17 21:57:04 +0530

    Refactor database connection and views

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,10 @@
 var path = require('path');
 var express = require('express');
 var router = express.Router();
-var client = require(path.join(__dirname,  '../', 'models/', 'database.js'));
+var connectionString = require(path.join(__dirname,  '../', 'config'));
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
-});
-
-router.get('/users', function(req, res, next) {
-  client.connect();
-  var query = client.query("SELECT name, address FROM users ORDER BY id ASC");
-  var results = [];
-  query.on('row', function(row) {
-    results.push(row);
-  });
-
-  // After all data is returned, close connection and return results
-  query.on('end', function() {
-      client.end();
-      return res.json(results);
-  });
-
-  // res.send('respond with a resource');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
+var path = require('path');
+var pg = require('pg');
+pg.defaults.ssl = true;
 var express = require('express');
 var router = express.Router();
-
+var connectionString = require(path.join(__dirname,  '../', 'config'));
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('', function(req, res, next) {
+  var results = [];
+  pg.connect(connectionString, function(err, client) {
+  if (err) throw err;
+    console.log('Connected to postgres! Getting schemas...');
+    client
+      .query("SELECT name, address FROM users ORDER BY id ASC")
+      .on('row', function(row) {
+        results.push(row);
+      })
+      .on('end', function() {
+        return res.json(results);
+      })
+  })
+});
+
+router.get('/new', function(req, res, next) {
+  res.render('users/new', { title: 'Registration' });
 });
 
 module.exports = router;
