efbe1d2370771787480516c259184e812e1d96bd
Harpal <hpanwar@grepruby.com>
2016-05-17 20:13:19 +0530

    Clear random color changes fully random

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -1,37 +1,6 @@
 $(document).ready(function() {
-  var i = 150 + rand(100);
-  var j = 150 + rand(100);
-  var k = 150 + rand(100);
-  var order = 'asc';
   setInterval(function(){
-    if (order == 'asc') {
-      if (i > 254 || j > 254 || k > 254) {
-        order = 'desc';
-        if (i < 1) { i = 1 } 
-        if (j < 1) { j = 1 }
-        if (k < 1) { k = 1 }
-      }else{
-        i += get_rand();
-        j += get_rand();
-        k += get_rand();
-      }
-    } 
-    else {
-      if (i < 1 || j < 1 || k < 1) {
-        order = 'asc';
-        if (i > 254) { i = 254 } 
-        if (j > 254) { j = 254 }
-        if (k > 254) { k = 254 }
-
-      }else{
-        i -= get_rand();
-        j -= get_rand();
-        k -= get_rand();
-      }
-
-
-    }
-    set_backgrount(i, j, k);
+    set_backgrount(get_rand(), get_rand(), get_rand());
   }, 3000);
 });
 
@@ -40,7 +9,7 @@ function set_backgrount(i, j , k) {
 }
 
 function get_rand() {
-  return rand(200)
+  return rand(255)
 }
 function rand(n) {
   return parseInt(Math.random()*n);
